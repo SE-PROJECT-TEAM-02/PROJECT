4938492ffd80054bdaa35d9048777cb4f306c38e
veeresh <veereshsakali962@gmail.com>
2024-04-09 08:13:08 +0530

    veeresh changes

--- Dreamscape Destinations/Source Code/Server/utils/auth.js
+++ Dreamscape Destinations/Source Code/Server/utils/auth.js
@@ -1,42 +1,42 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
- const secret = process.env.SECRET;
- const expiration = '2h';
+const secret =
+  "1B75482895F9E5A75740BE640E19DDC75B1D77F9D56D49F82F4674BA08F93F1D";
+const expiration = "2h";
 
- module.exports = {
-    signToken: function({ username, email, _id }) {
-        const payload = { username, email, _id };
+module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
 
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
+    console.log(secret);
 
-    authMiddleware: function({ req }) {
-        // allows token to be sent via req.body, req.query, or headers
-        let token = req.body.token || req.query.token || req.headers.authorization;
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 
-        // separate "Bearer" from "<tokenvalue>"
-        if (req.headers.authorization) {
-            token = token
-                .split(' ')
-                .pop()
-                .trim();
-        }
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-        // if no token, return request object as is
-        if (!token) {
-            return req;
-        }
-
-        try {
-            // decode & attach user data to request object
-            const { data } = jwt.verify(token, secret, { maxAge: expiration });
-            req.user = data;
-        } catch {
-            console.log('Invalid token');
-        }
-
-        // return updated request object
-        return req;
+    // separate "Bearer" from "<tokenvalue>"
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
     }
- };
+
+    // if no token, return request object as is
+    if (!token) {
+      return req;
+    }
+
+    try {
+      // decode & attach user data to request object
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    // return updated request object
+    return req;
+  },
+};
